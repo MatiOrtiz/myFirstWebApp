2aaff40f3e361a3be39fb828c8b7ed1fae2c25a9
Matías Nicolás Ortiz <88355010+MatiOrtiz@users.noreply.github.com>
2024-01-15 13:23:29 -0300

    HTMl page with jsp

--- src/main/java/com/masterspringspringboot/myfirstwebapp/hello/HelloWorldController.java
+++ src/main/java/com/masterspringspringboot/myfirstwebapp/hello/HelloWorldController.java
@@ -7,10 +7,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping("say-hello")
+  /*  @RequestMapping("say-hello-html")
     @ResponseBody
+    public String sayHelloHtml(){
+        StringBuffer sb= new StringBuffer();
+        sb.append("<html>");
+        sb.append("<title>");
+        sb.append("My WebApp");
+        sb.append("</title>");
+        sb.append("<body>");
+        sb.append("HTML page with JSP");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return sb.toString();
+    }
+    @RequestMapping("say-hello")
     public String sayHello(){
         return "Good day! Beautiful morning, no?";
+    }
+   */
+
+    @RequestMapping("say-hello-jsp")
+    public String sayHelloJsp(){
+        return "sayHello";
     }
 
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,8 @@
+<HTML>
+    <head>
+        <title>My First WebApp</title>
+    </head>
+    <body>
+        HTML page with JSP
+    </body>
+</HTML>
