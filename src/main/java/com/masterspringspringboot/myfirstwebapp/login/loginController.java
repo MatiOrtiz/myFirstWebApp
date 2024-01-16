5a47df68ee943ce1bc40b0b024838083dd6ad1df
Matías Nicolás Ortiz <88355010+MatiOrtiz@users.noreply.github.com>
2024-01-16 12:45:53 -0300

    welcome page added

--- src/main/java/com/masterspringspringboot/myfirstwebapp/login/loginController.java
+++ src/main/java/com/masterspringspringboot/myfirstwebapp/login/loginController.java
@@ -1,14 +1,24 @@
 package com.masterspringspringboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class loginController {
 
-    @RequestMapping("login")
+    @RequestMapping(value = "login", method= RequestMethod.GET)
     public String goToLoginPage(){
         return "login";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap modelMap){
+        modelMap.put("name", name);
+        modelMap.put("password", password);
+        return "welcome";
     }
 
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,11 @@
+<HTML>
+    <head>
+        <title>Welcome</title>
+    </head>
+    <body>
+        <div>Welcome to the page!</div>
+        <br>
+        Your name: ${name}
+        Your password: ${password}
+    </body>
+</HTML>
